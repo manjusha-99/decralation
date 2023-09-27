6b3aead4be120c09b47edcc4d3c9df70f4e4b4b6
manjusha-99 <146064294+manjusha-99@users.noreply.github.com>
2023-09-27 18:06:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+function varPlace(){
+    if(true){
+       var city="HYDERABAD";
+        console.log(city);
+        city="MUMBAI";
+        console.log(city);
+        city="BANGALORE";
+        console.log(city);
+        city="PUNE";
+    }
+    console.log(city);
+}
+varPlace();
+function letFlower(){
+    if(true){
+        let name="ROSES";
+        console.log(name);
+        name="LILLYS";
+        console.log(name);
+        name="HIBISCUS";
+        console.log(name);
+    }
+}
+letFlower();
+function constFood(){
+        const item="BURGER";
+    console.log(item);
+    }
+
+constFood();
+function city(){
+    if(true){
+        var x="hyderabad";
+        document.getElementById("label1").textContent=x;   
+    }
+}
+city();
+function flower(){
+    if(true){
+        let f="roses";
+        document.getElementById("label2").textContent=f;
+    }
+}
+flower();
+function food(){
+        const y="burger";
+        document.getElementById("label3").textContent=y;
+}
+food();
+
+
+
+
+
+
+
+
+
